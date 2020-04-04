c82572b33044cae6810882ef39f75ab025b85e13
Kirsten Keels <keelsk@gmail.com>
2020-04-04 21:31:15 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
+    binding.pry
     if is_logged_in?(session)
       @tweets = Tweet.all
       @user = User.find_by(session[:user_id])
