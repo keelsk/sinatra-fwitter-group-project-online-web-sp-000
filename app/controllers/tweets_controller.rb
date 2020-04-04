b967c05ce5f8f2a7462af27e82d5d7131dbb1e06
Kirsten Keels <keelsk@gmail.com>
2020-04-04 22:37:14 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,6 +14,7 @@ class TweetsController < ApplicationController
   end
 
   post '/tweets' do
+    redirect 'tweets/new' if params[:content] == ""
     @user = User.find(session[:user_id])
     @tweet = @user.tweets.create(content: params[:content])
     redirect '/tweets'
