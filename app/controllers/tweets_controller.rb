46bc79026ac669c97f1430d124b4518c68e296e5
Kirsten Keels <keelsk@gmail.com>
2020-04-04 19:58:10 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,8 @@
 class TweetsController < ApplicationController
 
-  get '/tweets' do 
+  get '/tweets' do
     @tweets = Tweet.all
+    @user = User.find_by_id(session[:user_id])
     erb :'tweets/index'
   end
 end
