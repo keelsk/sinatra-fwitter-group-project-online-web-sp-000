653f620a34116603cefd233c952e876ffd2acfb9
Kirsten Keels <keelsk@gmail.com>
2020-04-04 21:31:43 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,6 @@ class TweetsController < ApplicationController
   get '/tweets' do
     binding.pry
     if is_logged_in?(session)
-      @tweets = Tweet.all
       @user = User.find_by(session[:user_id])
       erb :'tweets/index'
     else
