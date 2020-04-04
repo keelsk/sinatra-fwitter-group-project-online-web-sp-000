ae58965feeaaea81bf4452b9ad425d5d9d50ddf4
Kirsten Keels <keelsk@gmail.com>
2020-04-04 19:35:58 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   post '/signup' do
     user = User.create(params)
-    if user && user.authenticate(params[:password])
+    if user.save && user.authenticate(params[:password])
       session[:user_id] = user[:id]
       redirect '/tweets'
     end
