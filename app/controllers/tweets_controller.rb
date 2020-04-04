17a094451333ebc57ea63fcd097f33f94cee6483
Kirsten Keels <keelsk@gmail.com>
2020-04-04 22:03:38 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,7 +7,7 @@ class TweetsController < ApplicationController
     erb :'tweets/index'
   end
 
-  get 'tweets/new' do
+  get '/tweets/new' do
     redirect to '/login' if !logged_in?
     @user = User.find_by(session[:user_id])
     erb :'tweets/new'
