62ac53418300fb06089f9b416e71dbc9704fe43a
Kirsten Keels <keelsk@gmail.com>
2020-04-04 21:55:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    enable :Sessions
+    enable :sessions
     set :session_secret, "fwitter"
     set :public_folder, 'public'
     set :views, 'app/views'
