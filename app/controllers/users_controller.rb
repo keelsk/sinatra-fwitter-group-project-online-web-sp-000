637797e41b8b8bd80259f0fe289d3c7f1bf58c12
Kirsten Keels <keelsk@gmail.com>
2020-04-04 19:01:36 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
+require 'pry'
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+  post '/signup' do
+    @user = User.create(params)
+    binding.pry
+  end
 
 end
