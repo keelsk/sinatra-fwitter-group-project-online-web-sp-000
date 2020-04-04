fccb88dc62e0e8c91e137104141e0f6869ab3062
Kirsten Keels <keelsk@gmail.com>
2020-04-04 20:05:01 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   post '/login' do
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect '/tweets'
     end
     redirect '/login'
