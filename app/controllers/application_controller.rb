c1f611486233674131243fe7440862e549af8be4
Kirsten Keels <keelsk@gmail.com>
2020-04-04 23:35:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,10 @@ class ApplicationController < Sinatra::Base
     def logged_in?
       !!session[:user_id]
     end
+
+    def current_user 
+      @user = User.find(session[:user_id])
+    end
   end
 
 end
